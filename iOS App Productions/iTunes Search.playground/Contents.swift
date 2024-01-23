e941cbc0d40bb58fdc842bd559b37982f2aa4d49
David Bradshaw <dabrad26@me.com>
2024-01-22 23:48:00 -0800

    fix: app production 2.5 playground

--- iOS App Productions/iTunes Search.playground/Contents.swift
+++ iOS App Productions/iTunes Search.playground/Contents.swift
@@ -1,26 +1,108 @@
 import UIKit
 
-var itunesApi = URLComponents(string: "https://itunes.apple.com/search")!
-itunesApi.queryItems = [
-    "term": "Harry Potter",
-    "media": "movie",
-    "limit": "1",
-].map { URLQueryItem(name: $0.key, value: $0.value) }
-
-
-Task {
-    let (data, response) = try await URLSession.shared.data(from: itunesApi.url!)
-    let httpResonse = response as? HTTPURLResponse;
-    
-    if (httpResonse?.statusCode == 200) {
-        let dataString = String(data: data, encoding: .utf8)
+extension Data {
+    func prettyPrintedJSONString() {
+        guard 
+            let jsonObject = try? JSONSerialization.jsonObject(with: self, options: []),
+            let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted]),
+            let prettyJSONString = String(data: jsonData, encoding: .utf8) else {
+            print("Failed to make JSON pretty")
+            return
+        }
         
-        print(dataString!)
-    } else {
-        let dataString = String(data: data, encoding: .utf8)
-        
-        print("API call failed!")
-        print(dataString!)
+        print(prettyJSONString)
     }
 }
 
+struct StoreItem: Codable {
+    var artistName: String?
+    var trackName: String?
+    var kind: String?
+    var artworkURL: URL?
+    var genre: String?
+    var description: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case artistName
+        case trackName
+        case kind
+        case artworkURL = "artworkUrl60"
+        case description
+        case genre = "primaryGenreName"
+    }
+    
+    enum AdditionalKeys: String, CodingKey {
+        case longDescription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        artistName = try values.decode(String.self, forKey: CodingKeys.artistName)
+        trackName = try values.decode(String.self, forKey: CodingKeys.trackName)
+        kind = try values.decode(String.self, forKey: CodingKeys.kind)
+        artworkURL = try values.decode(URL.self, forKey:
+           CodingKeys.artworkURL)
+        
+        if let genre = try? values.decode(String.self,
+           forKey: CodingKeys.genre) {
+            self.genre = genre
+        }
+        
+        if let description = try? values.decode(String.self,
+           forKey: CodingKeys.description) {
+            self.description = description
+        } else {
+            let additionalValues = try decoder.container(keyedBy:
+               AdditionalKeys.self)
+            description = (try? additionalValues.decode(String.self,
+               forKey: AdditionalKeys.longDescription))
+        }
+    }
+}
+
+struct SearchResponse: Codable {
+    let results: [StoreItem]
+}
+
+enum SearchResponseError: Error, LocalizedError {
+    case errorResponse
+}
+
+func fetchItems(matching query: [String: String]) async throws -> [StoreItem] {
+    var itunesApi = URLComponents(string: "https://itunes.apple.com/search")!
+    itunesApi.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+    
+    let (data, response) = try await URLSession.shared.data(from: itunesApi.url!)
+    
+    guard let httpResponse = response as? HTTPURLResponse,
+          httpResponse.statusCode == 200 else {
+        throw SearchResponseError.errorResponse
+    }
+    
+//    print(data.prettyPrintedJSONString())
+    let jsonDecoder = JSONDecoder()
+    let results = try jsonDecoder.decode(SearchResponse.self, from: data)
+    return results.results
+}
+
+Task {
+    do {
+        // Possible media: `music`, `ebook`, `movie`
+        let results = try await fetchItems(matching: ["term": "Apple", "media": "movie"])
+        results.forEach { item in
+            print("""
+            ============
+                Artist name: \(item.artistName ?? "NO VALUE")
+                Track name: \(item.trackName ?? "NO VALUE")
+                Kind: \(item.kind ?? "NO VALUE")
+                Genre: \(item.genre ?? "NO VALUE")
+                Description: \(item.description ?? "NO VALUE")
+                Artwork URL: \(item.artworkURL?.absoluteString ?? "NO VALUE")
+            ============
+            """)
+            
+        }
+    } catch {
+        print(error)
+    }
+}
